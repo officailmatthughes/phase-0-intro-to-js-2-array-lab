7a66b4f08b69fc5633b4fc5a3e6ec3fe73b970b7
Mattherw <officialmatthughes>
2022-05-01 17:32:22 -0700

    commit tree 06f8b13f73e6328998541b481ea55f44b3562231

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(cat) {
+    cats.pop(cat);
+}
+
+function destructivelyRemoveFirstCat(cat) {
+    cats.shift(cat)
+}
+
+function appendCat(name) {
+const allCats = [...cats, name]    
+    return allCats;
+}
+
+function prependCat(name) {
+    const allCats = [name, ...cats]
+    return allCats;
+}
+
+function removeLastCat() {
+    const allCats = cats.slice(0, cats.length - 1)
+    return allCats;
+}
+
+function removeFirstCat() {
+    const allCats = cats.slice(1);
+    return allCats;
+}
+    
